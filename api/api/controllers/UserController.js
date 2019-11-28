6c380fba117c6e636e9eb2dc59dd9310faa6855a
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-11-28 12:02:52 -0300

    encrypting passwords with bcrypt

--- api/api/controllers/UserController.js
+++ api/api/controllers/UserController.js
@@ -6,13 +6,35 @@
  */
 
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 module.exports = {
   
-  login: function(req, res) {
+  create: async function(req, res) {
+    const crypt = await bcrypt.hash(req.body.password, 10);
+    const newUser = {
+      username: req.body.username,
+      password: crypt,
+    }
+
+    console.log(typeof(newUser.password));
+    
+    User.create(newUser)
+      .then(msg => {
+        return res.status(200).json({
+          msg: 'User created!',
+        });
+      })
+      .catch(err => {
+        return res.status(400).json(err);
+      })
+  },
+
+  login: async function(req, res) {
     User.findOne({username: req.body.username})
-      .then(user => {
-        if (user.username === req.body.username) {
+      .then(async user => {
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if (match) {
           res.status(200).json({
             tolken: jwt.sign(user.toJSON(), 'secret'),
           })
